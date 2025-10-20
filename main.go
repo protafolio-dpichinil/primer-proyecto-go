3a821fcf4087b34ac1e9680c380a1f5babfacfe8
dpichinil <daniel.pichinil@gmail.com>
2025-10-20 04:03:36 +0000

    transform application in webservice

--- main.go
+++ main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+// helloHandler responde a las peticiones HTTP en la ruta que le asignemos.
+// Recibe un http.ResponseWriter para escribir la respuesta y un http.Request con la información de la petición.
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	// Nos aseguramos de que solo respondemos a peticiones GET.
+	if r.Method != http.MethodGet {
+		http.Error(w, "Método no permitido", http.StatusMethodNotAllowed)
+		return
+	}
+
+	fmt.Fprintln(w, "¡Hola desde el microservicio Go!")
+}
 
 func main() {
-	fmt.Println("¡Hola desde Go en un Dev Container!")
+	// Registramos nuestra función `helloHandler` para que maneje las peticiones a la ruta "/".
+	http.HandleFunc("/", helloHandler)
+
+	fmt.Println("Servidor escuchando en http://localhost:8080")
+	// Iniciamos el servidor en el puerto 8080. log.Fatal se ejecutará si hay un error al iniciar.
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
